d3acb87d484d30b02eeda1e9237aa99b0a9c9cff
TomParkwood <39772727+TomParkwood@users.noreply.github.com>
2018-07-30 16:54:51 +0100

    Sharp Range
    Holy shit is this going to be difficult trying to hide some of the data

--- Sharp-range.php
+++ Sharp-range.php
@@ -1,1 +1,62 @@
 <!--Sharp Range-->
+<?php
+$pageTitle = "The Sharp Range";
+$pageSummary = "The four different ranges currently offered by Sharp, with machines going from smaller desktop machines all the way to high speed machines with up to 75 pages per minute";
+$metaKeywords = "Keywords";
+$metaDescription= "Description";
+include('include/pds-main.php');
+include('include/pds-header.php');
+?>
+<div id="crumbs-wrapper">
+  <div id="crumbs">
+    You are at: <a href="index.php">Home</a> &gt; <a href="products.php">Products</a> &gt; <a href="sharp-range.php">Sharp Range</a>
+  </div>
+</div>
+<div id="main-wrapper">
+  <div id="equal-1" class="width-three">
+    <h1>The Sharp Range of MFDs</h1>
+    <table style="width:100%;" border="1">
+      <tr>
+        <td style="width:50%;"><strong>The Griffin Range</strong></td>
+        <td style="width:50%;"><strong>The Neo Range</strong></td>
+      </tr>
+      <tr>
+        <td>A range of 5 SRA3 machines with varying speeds from 30 pages per minute up to 60 pages per minute, with plenty of finishing and software options. Very easy to use and super reliable.</td>
+        <td>A pair of A4 colour devices for the small office or low user. Running speeds of 25 or 30 pages per minute with all the bells and whistles expected from a much larger machine.</td>
+      </tr>
+      <tr>
+        <td>See all of the details <a href="griffin.php">here</a>.<br>
+          a href to griffin page then link the image for the hyperlink.
+        </td>
+        <td>See all of the details here.<br>
+          a href to neo page then link the image for the hyperlink.
+        </td>
+      </tr>
+    </table>
+    <br>
+    <table style="width:100%;" border="1">
+      <tr>
+        <td style="width:50%;"><strong>The Sphinx Range</strong></td>
+        <td style="width:50%;"><strong>The Polaris Range</strong></td>
+      </tr>
+      <tr>
+        <td>The MX-2630N is ideal for the offices who want the functionality of a small MFP but with the options of a higher range such as more trays and finishing options.</td>
+        <td>A pair of high speed machines with speed of up to 75 pages per minute with a wide range of finishing options. Need high productivity and quality machine then look no further than the Polaris range.</td>
+      </tr>
+      <tr>
+        <td>See all of the details here.<br>
+          a href to sphinx page then link the image for the hyperlink.
+        </td>
+        <td>See all of the details here.<br>
+         a href to polaris page then link the image for the hyperlink.
+       </td>
+      </tr>
+    </table>
+  </div>
+  <?php
+  include('include/pds-sidebar.php');
+   ?>
+</div>
+<?php
+include('include/pds-footer.php');
+ ?>
---
+++ griffin.php
@@ -0,0 +1,60 @@
+<!--Griffin-->
+<?php
+$pageTitle = "Griffin Range";
+$pageSummary = "Summary";
+$metaKeywords = "keywords";
+$metaDescription = "description";
+include('include/pds-main.php');
+include('include/pds-header.php');
+ ?>
+<div id="crumbs-wrapper">
+  <div id="crumbs">
+    You are at: <a href="index.php">Home</a> &gt; <a href="products.php"> Products</a> &gt; <a href="sharp-range.php">Sharp Range</a> &gt; <a href="griffin.php">Griffin</a>
+  </div>
+</div>
+<div id="main-wrapper">
+  <div id="equal-1" class="width-three">
+    <h1>The Griffin Range</h1>
+    <h3>General Information</h3>
+      <table style="width:100%;">
+        <tr>
+          <td style="width:75%;">The Griffin range of A3 colour devices offer a range of options and upgrades making it adaptable to your future needs. The optimise productivity and are ideal for any busy workgroup that needs high performance, high quality colour and versatility.<br>
+            The standard Print Release function allows users to send and store documents to a main MFP, which acts as a server, allowing print jobs to be securely released at up to five connected network-ready devices. Optional workflow enhancements include Single Sign-On cloud connectivity, mobile connectivity via Wi-Fi and USB direct printing of MS Office files.<br>
+            A tiltable 10.1" colour LCD touchscreen provides the ability to drag and drop frequently used icons for simple customisation of the home screen. Everyday operation is simple and intuitive; just touch, tap, pinch and spread or swipe for instant access to every feature, function and setting including an advanced preview feature.<br>
+            Print and copy sppeds of up to 60 pages per minute in colour and black &amp; white, a built-in 100-sheet Reversing Single Pass Feeder and a scan speed of up to 80 ipm, all help boost efficiency and productivity; while 600 x 600 dpi print resolution, combined with Sharp's auto process control and developer refresh system ensure consistently high image quality. A maximum paper capacity of 6,300 sheets and support for a wide range of media (up to SRA3 and 300gsm) enables this device to handle varied output requirements with ease. And a varied selection of finishing options such as saddle stiching, manual and stapleless stapling add further versatility.<br>
+            The Energy Star certified devices uses Sharp's latest energy-saving technology. A rapid warm up time, low power consumption and low TEC value all aim to save money and support your environmental objectives. In addition, the automatic toner cartridge eject function ensures that all toner has beem consumed before it can be replaced, therby saving resources.</td>
+          <td style="width:25%;">Image goes here</td>
+        </tr>
+      </table>
+      <br>
+    <h3>Specifications</h3>
+      <table style="width:100%; border:1px solid #000000; border-collapse:collapse;padding:10px;" border="1">
+        <tr>
+          <td style="width:50%;">Engine speed Colour ppm (A4)</td>
+          <td style="width:50%;">30/ 35/ 40/ 50/ 60</td>
+        </tr>
+        <tr>
+          <td style="width: 179px;">Engine speed B/W ppm (A4)</td>
+          <td style="width: 158.333px;">30/ 35/ 40/ 50/ 60</td>
+        </tr>
+        <tr>
+          <td style="width: 179px;">Engine speed Colour ppm (A3)</td>
+          <td style="width: 158.333px;">16/ 18/ 20/ 26/ 30</td>
+        </tr>
+        <tr>
+          <td style="width: 179px;">Engine speed B/W ppm (A3)</td>
+          <td style="width: 158.333px;">16/ 18/ 20/ 26/ 30</td>
+        </tr>
+        <tr>
+          <td style="width: 179px;">Paper size: Min.-Max.</td>
+          <td style="width: 158.333px;">A5 - SRA3</td>
+        </tr>
+      </table>
+  </div>
+<?php
+include('include/pds.sidebar.php');
+?>
+</div>
+<?php
+include('include/pds-footer.php');
+?>
