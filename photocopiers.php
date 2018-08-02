183e99daca6e7db008bccf86da177ac363d7bef4
TomParkwood <39772727+TomParkwood@users.noreply.github.com>
2018-08-02 14:34:53 +0100

    Photocopiers

---
+++ photocopiers.php
@@ -0,0 +1,45 @@
+<!--Photocopiers-->
+<?php
+$pageTitle="Photocopiers, Printers and MFDs:";
+$pageSummary="Multifunctional devices have evolved from photocopiers and have many areas of functionality, predominantly printing, faxing, scanning, the ability to e-mail documents and integration with other applciations";
+$metaKeywords="Keywords";
+$metaDescription="Description";
+include('include/pds-main.php');
+include('include/pds-header.php');
+?>
+<div id="crumbs-wrapper">
+  <div id="crumbs">
+    You are at: <a href="index.php">Home</a> &gt; <a href="products.php">Products</a> &gt; <a href="photocopiers.php">Photocopiers</a>
+  </div>
+</div>
+<div id="main-wrapper">
+  <div id="equal-1" class="width-three">
+    <div class="main-span">
+      <h1>Photocopiers, Printers &amp; MFD's</h1>
+      <h2>The Sharp Range</h2>
+      <p>The Sharp range consists of four different types of machines, which go from smaller desktop machines all the way to high speed machines with up to 75 pages per minute.</p>
+        <table style="width:100%;">
+          <tr>
+            <td style="width:50%;"><strong>The Griffin Range</strong></td>
+            <td style="width:50%;"><strong>The Neo Range</strong></td>
+          </tr>
+          <tr>
+            <td>Place image here with a hyperlink to the page</td>
+            <td>Place image here with a hyperlink to the page</td>
+          </tr>
+        </table>
+        <br>
+        <table style="width:100%;">
+          <tr>
+            <td style="width:50%;"><strong>The Sphinx Range</strong></td>
+            <td style="width:50%;"><strong>The Polaris Range</strong></td>
+          </tr>
+          <tr>
+            <td>Place image here with a hyperlink to the page</td>
+            <td>Place image here with a hyperlink to the page</td>
+          </tr>
+        </table>
+        <br>
+        <br>
+      <h2>The Utax Range</h2>
+      <p>The Utax
