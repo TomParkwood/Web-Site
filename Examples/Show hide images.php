968111c19e708b29aec962ca628df353ff064229
TomParkwood <39772727+TomParkwood@users.noreply.github.com>
2018-09-07 12:37:45 +0100

    Examples Folder
    This maybe helpful for referring back to when i have almost forgotten how to breathe

---
+++ Examples/Show hide images.php
@@ -0,0 +1,15 @@
+<div id="ShowHide" style="display:none;">
+	<img src="images/utax/2506cioptions.jpg">
+</div>
+<script type="text/JavaScript">
+	function tend(){
+		document.getElementById('ShowHide').style.display= 'block';
+	}
+	function tn(){
+		document.getElementById('ShowHide').style.display= 'none';
+	}
+</script>
+<input type="buttton" onclick="tend()" value="show">
+<input type="button" onclick="tn()" value="none">
+
+<!--Show and hide entire Div sections while not reserving the space for them-->
